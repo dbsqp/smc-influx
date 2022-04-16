77d074558ed855ec46bd84d03585db5e96c4177f
dbsqp <mjp@laptop.local>
2022-04-16 13:51:19 +0200

    update

--- smc-influxdb.c
+++ smc-influxdb.c
@@ -22,15 +22,71 @@
 #include <string.h>
 #include <IOKit/IOKitLib.h>
 
-#include "smc-influxdb.h"
-
 #include <unistd.h>
 #include <time.h>
 
 static io_connect_t conn;
 
+#define KERNEL_INDEX_SMC 2
 
+#define SMC_CMD_READ_BYTES 5
+#define SMC_CMD_WRITE_BYTES 6
+#define SMC_CMD_READ_INDEX 8
+#define SMC_CMD_READ_KEYINFO 9
+#define SMC_CMD_READ_PLIMIT 11
+#define SMC_CMD_READ_VERS 12
 
+// key values
+typedef struct {
+    char major;
+    char minor;
+    char build;
+    char reserved[1];
+    UInt16 release;
+} SMCKeyData_vers_t;
+
+typedef struct {
+    UInt16 version;
+    UInt16 length;
+    UInt32 cpuPLimit;
+    UInt32 gpuPLimit;
+    UInt32 memPLimit;
+} SMCKeyData_pLimitData_t;
+
+typedef struct {
+    UInt32 dataSize;
+    UInt32 dataType;
+    char dataAttributes;
+} SMCKeyData_keyInfo_t;
+
+typedef char SMCBytes_t[32];
+
+typedef struct {
+    UInt32 key;
+    SMCKeyData_vers_t vers;
+    SMCKeyData_pLimitData_t pLimitData;
+    SMCKeyData_keyInfo_t keyInfo;
+    char result;
+    char status;
+    char data8;
+    UInt32 data32;
+    SMCBytes_t bytes;
+} SMCKeyData_t;
+
+typedef char UInt32Char_t[5];
+
+typedef struct {
+    UInt32Char_t key;
+    UInt32 dataSize;
+    UInt32Char_t dataType;
+    SMCBytes_t bytes;
+} SMCVal_t;
+
+// prototypes
+double SMCGetTemperature(char* key);
+int SMCGetFanRpm(char* key);
+
+// function
 UInt32 _strtoul(char* str, int size, int base)
 {
     UInt32 total = 0;
